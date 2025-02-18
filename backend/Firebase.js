5cb9eccaae038e7141b7d203609f027dcb077505
apc6360 <apc6360@psu.edu>
2025-02-18 13:18:03 -0500

    firebase

--- backend/Firebase.js
+++ backend/Firebase.js
@@ -4,29 +4,20 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = process.env.NODE_ENV === 'production' ? {
-//   apiKey: "",
-//   authDomain: "",
-//   projectId: "",
-//   storageBucket: "",
-//   messagingSenderId: "",
-//   appId: "", 
-//   measurementId: ""
-// } : {
-//   apiKey: "",
-//   authDomain: "",
-//   projectId: "",
-//   storageBucket: "com",
-//   messagingSenderId: "",
-//   appId: "",
-//   measurementId: ""
-// }
+const firebaseConfig = {
+    apiKey: "AIzaSyCBw_hMAwgat_mOyhzK2ZigQS4nn0VO8KI",
+    authDomain: "roomielife-18adb.firebaseapp.com",
+    projectId: "roomielife-18adb",
+    storageBucket: "roomielife-18adb.firebasestorage.app",
+    messagingSenderId: "1076561208623",
+    appId: "1:1076561208623:web:5482b640ca54f475d7adda"
+  };
 
 // // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-// export const storage = getStorage(app);
-// export const database = getFirestore(app);
-// export const analytics = () => getAnalytics(app);
+ const app = initializeApp(firebaseConfig);
+ export const auth = getAuth(app);
+ export const storage = getStorage(app);
+ export const database = getFirestore(app);
+ export const analytics = () => getAnalytics(app);
 
 // export default app
